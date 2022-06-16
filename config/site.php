f71b50e5dc22b8281e37c6a9fd151828a6bf4a8a
Andrés Bedoya <andres@bedoya.co>
2022-06-16 16:12:59 -0500

    Removed nonce and success keys from the array and also renamed the key for the checkout url to checkout_url

--- config/site.php
+++ config/site.php
@@ -1,8 +1,7 @@
 <?php
+
 return [
-    'nonce' => env('APP_NONCE', 'Andrés Bedoya'),
     'login' => '6dd490faf9cb87a9862245da41170ff2',
     'key' => '024h1IlD',
-    'success' => '',
-    'checkout' => 'https://dev.placetopay.com/redirection',
+    'checkout_url' => 'https://dev.placetopay.com/redirection/',
 ];
