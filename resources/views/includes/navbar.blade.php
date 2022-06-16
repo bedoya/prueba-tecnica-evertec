1d4b130cf0960ee768d3084b7d729a63e184d766
Andrés Bedoya <andres@bedoya.co>
2022-06-16 12:23:26 -0500

    Added the correct links to the navbar and also made the "active" class dynamic according to the Route.

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -7,18 +7,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">{{ __('Inicio') }}</a>
+                    <a class="nav-link @if(Route::is('home')) active @endif" aria-current="page" href="{{ route('home') }}">
+                        {{ __('Inicio') }}
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">{{ __('Productos') }}</a>
+                    <a class="nav-link @if(Route::is('products.index')) active @endif" href="{{ route('products.index') }}">{{ __('Productos') }}</a>
                 </li>
                 @can('manage-store')
                     <li class="nav-item">
-                        <a class="nav-link" href="#">{{ __('Todas las órdenes') }}</a>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">{{ __('Mis órdenes') }}</a>
+                        <a class="nav-link @if(Route::is('orders.index')) active @endif" href="#">{{ __('Todas las órdenes') }}</a>
                     </li>
                 @endcan
             </ul>
