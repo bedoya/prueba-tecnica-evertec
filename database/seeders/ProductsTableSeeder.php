a72a1f08341bf513f803d969d5805ceeada66b1d
Andrés Bedoya <andres@bedoya.co>
2022-06-16 16:14:11 -0500

    Updated the price of the product to 50000, as the Webcheckout has a lower limit and the 500 COP price was not supported.

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -17,7 +17,7 @@ class ProductsTableSeeder extends Seeder
         $products = [
             [
                 'name' => 'Producto 1',
-                'price' => 500,
+                'price' => 50000,
                 'data' => [
                     'excerpt' => 'Esta es la descripción corta del producto 1',
                     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia mi turpis, in elementum velit cursus at. Proin massa enim, tristique at sem sit amet, tempor ultrices tortor. Nulla egestas nulla at nisl consectetur consectetur. Fusce at elit a sem condimentum dictum eget quis justo. Maecenas scelerisque consectetur viverra. Integer est leo, ultricies a consequat vitae, tincidunt et odio. Phasellus facilisis commodo velit, tristique porta mauris euismod id. Maecenas sed iaculis quam.',
