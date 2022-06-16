9f8a80067b3caedbea3240d376ede2186dd50d80
Andrés Bedoya <andres@bedoya.co>
2022-06-16 08:37:33 -0500

    Added the index and show views to the project. Both are very basic and could be inproved at a later stage.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,3 +1,29 @@
 <x-layout>
-
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col" class="text-center">#</th>
+            <th scope="col" class="text-center">{{ __('Nombre') }}</th>
+            <th scope="col" class="text-center">{{ __('Descripción') }}</th>
+            <th scope="col" class="text-center">{{ __('Precio') }}</th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($products as $product)
+            <tr class="align-middle">
+                <th scope="row" class="text-end">{{ $product->id }}</th>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->getData('excerpt') }}</td>
+                <td class="text-end">{{ number_format($product->price, 2, ',', '.') }}</td>
+                <td class="text-center">
+                    <a href="{{ route('products.show', [$product]) }}" type="button" class="btn btn-sm btn-primary">
+                        <i class="fa-solid fa-eye me-2"></i>
+                        {{ __('Detalle') }}
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </x-layout>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,3 +1,17 @@
 <x-layout>
-    Product
+    <div class="card">
+        <div class="card-header">
+            <h5 class="card-title">{{ $product->name }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ $product->getData('excerpt') }}</h6>
+        </div>
+        <div class="card-body">
+            <p class="card-text">{{ $product->getData('description') }}.</p>
+        </div>
+        <div class="card-footer text-end">
+            <a href="{{ route('orders.create', product detail[$product]) }}" class="btn btn-primary">
+                <i class="fa-solid fa-cart-shopping me-2"></i>
+                {{ __('Comprar') }}
+            </a>
+        </div>
+    </div>
 </x-layout>
