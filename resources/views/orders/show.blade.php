9ff6c075b93a2e4910cf29a446727f92659fd20e
Andrés Bedoya <andres@bedoya.co>
2022-06-16 19:43:50 -0500

    Display the information of an order after it has been processed

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -13,10 +13,12 @@
                 <div class="col-lg-9 py-2"><strong>{{ $order->client->customer_mobile }}</strong>
                 </div>
             </div>
-            <div class="row border border-dark rounded p-2">
+            <div
+                class="row rounded bg-opacity-25 @if($order->hasStatus('created')) bg-warning @elseif($order->hasStatus('rejected')) bg-danger @elseif($order->hasStatus('payed')) bg-success @endif p-2">
                 <div class="col-lg-3 offset-lg-1">
                     <span class="d-block text-center">{{ __('Fecha de la compra') }}</span>
-                    <span class="d-block text-center"><strong>{{ $order->created_at->format('Y-m-d h:i:s') }}</strong></span>
+                    <span
+                        class="d-block text-center"><strong>{{ $order->created_at->format('Y-m-d h:i:s') }}</strong></span>
                 </div>
                 <div class="col-lg-3">
                     <span class="d-block text-center">{{ __('Estado del pago') }}</span>
@@ -24,7 +26,8 @@
                 </div>
                 <div class="col-lg-3">
                     <span class="d-block text-center">{{ __('# de transacción') }}</span>
-                    <span class="d-block text-center"><strong>{{ $order->getData('transaction.request_id') }}</strong></span>
+                    <span
+                        class="d-block text-center"><strong>{{ $order->getData('transaction.request_id') }}</strong></span>
                 </div>
             </div>
             <div class="table-responsive">
@@ -56,5 +59,28 @@
                 </table>
             </div>
         </div>
+        @if($order->hasStatus('Rejected'))
+            <div class="card-footer text-end">
+                <a href="{{ route('orders.edit', [$order]) }}" class="btn btn-primary">
+                    <i class="fa-solid fa-cart-shopping me-2"></i>
+                    {{ __('Intentar nuevamente') }}
+                </a>
+            </div>
+        @elseif($order->hasStatus('Created') && ($order->getData('transaction.request_id') != null))
+            <div class="card-footer text-muted align-items-center justify-content-between d-flex">
+                <div>
+                    <i class="fa-solid fa-clock-rotate-left me-2"></i>
+                    {{ __('La aprobación de su orden puede tardar algún tiempo, espere unos minutos y recargue esta página') }}
+                </div>
+                <form method="post" action="{{ route('orders.update', [$order]) }}" class="p-0 m-0 d-inline-block float-end">
+                    @csrf
+                    @method('patch')
+                    <button type="submit" class="btn btn-primary float-end">
+                        <i class="fa-solid fa-cart-shopping me-2"></i>
+                        {{ __('Reintentar') }}
+                    </button>
+                </form>
+            </div>
+        @endif
     </div>
 </x-layout>
